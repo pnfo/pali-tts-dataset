63bb358249b449f8b447e1944269df00ce5d12a6
pathnirvana <pathnirvana@gmail.com>
2023-04-18 18:35:28 +0700

    misc changes

--- create-dataset.js
+++ create-dataset.js
@@ -10,7 +10,9 @@ import {sinhalaToRomanConvert} from '@pnfo/singlish-search/roman_convert.js'
 const labelInputFolder = '/Users/janaka/node/tipitaka.lk/public/audio', // also in '/Volumes/1TB/audio/final uploaded'
     textInputFolder = '/Users/janaka/node/tipitaka.lk/public/static/text',
     audioInputFolder = '/Volumes/1TB/audio/silence-added'
-const minClipLength = 3, maxClipLength = 25
+const minClipLength = 3, maxClipLength = 15
+// TODO: ap- should not have any gatha, force set to para/default - but dhs was 
+const forceTypeNoGatha = /^(ap-dhs)/g
 
 const fileMap = JSON.parse(fs.readFileSync(path.join(labelInputFolder, 'file-map.json'), 'utf-8'))
 
@@ -59,6 +61,8 @@ Object.entries(fileMap).forEach(([textFile, labelFiles]) => {
     })
     labeled = labeled.filter(e => e.label) // some text files could be only partially recorded
 
+    if (forceTypeNoGatha.test(textFile)) labeled.forEach(e => e.type = (e.type == 'gatha') ? 'paragraph' : e.type)
+
     // get only the labels of length within desired range
     let usable = labeled.filter((e, i) => e.label && e.label.length > minClipLength && e.label.length <= maxClipLength)
     usable.forEach(e => {
@@ -94,12 +98,12 @@ usableEntries.forEach(e => {
     e.score = getMedianWordFrequency(e.words)
     e.lengthRatio = e.label.length / e.roman.replace(/h/g, '').length
 })
-const outliersToRemove = 200
+const outliersToRemove = 100
 const outlierRemoved = usableEntries.sort((a, b) => a.lengthRatio - b.lengthRatio).slice(outliersToRemove, -outliersToRemove)
 outlierRemoved.sort((a, b) => a.score - b.score) // ascending order of the score
 const outlierLength = outlierRemoved.reduce((acc, e) => acc + e.label.length, 0)
 
-const requiredLength = 20 * 3600  // collect until this many hours are reached
+const requiredLength = 10 * 3600  // collect until this many hours are reached
 let collectedLength = 0
 const usedEntries = outlierRemoved.filter((e, i) => {
     collectedLength += e.label.length
@@ -110,7 +114,7 @@ usedEntries.sort((a, b) => a.lengthRatio - b.lengthRatio)
 
 // extract content from audio files
 // trim all silences more than 0.75 seconds, normalize and set rate (original flac is 44100)
-const outputFolder = 'wavs', outputOptions = 'silence -l 1 0.1 1% -1 0.75 1% reverse silence 1 0.1 1% reverse rate 22050 norm -1' //silence -l 1 0.1 1% -1 0.75 1%
+const outputFolder = 'wavs', outputOptions = 'silence -l 1 0.1 1% -1 0.75 1% reverse silence 1 0.1 1% reverse rate 22050 norm -1' //rate 22050 before norm
 fs.rmSync(outputFolder, {recursive: true})
 fs.mkdirSync(outputFolder)
 
@@ -120,12 +124,12 @@ const extractSegment = (e, callback) => { // Define the function that will extra
     exec(command, callback);
 }
 const startTime = new Date()
-async.mapLimit(usedEntries, 7, (e, mapCallback) => {
-        extractSegment(e, (error, stdout, stderr) => mapCallback(error || null, stderr || stdout))
-    }, (error, results) => {
-        if (error) console.error(error)
-        console.log(`Extracted audio from flac files in ${((Date.now() - startTime) / 1000).toFixed(2)} seconds`);
-    })
+// async.mapLimit(usedEntries, 7, (e, mapCallback) => {
+//         extractSegment(e, (error, stdout, stderr) => mapCallback(error || null, stderr || stdout))
+//     }, (error, results) => {
+//         if (error) console.error(error)
+//         console.log(`Extracted audio from flac files in ${((Date.now() - startTime) / 1000).toFixed(2)} seconds`);
+//     })
 
 
 // compute character and type counts
